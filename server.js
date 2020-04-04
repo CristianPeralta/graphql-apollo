d7319b3094caf2a6b03b4139a8214ec5689534b1
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-04 15:31:46 -0500

    add endpoint for root endpoint

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const server = express();
 
+server.get("/", (req, res) => {
+    res.send("<html><h1>Express</h1> </html>")
+})
+
 server.listen(3000, () => {
     console.log('Listening on port 3000');
 });
