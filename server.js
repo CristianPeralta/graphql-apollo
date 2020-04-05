2a7c9ffdf05dec9a0b954df591301c5a595d59ce
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-05 09:45:41 -0500

    install mongoose and configure connection

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import mongoose from 'mongoose';
 import schema from './schema';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/graphqltestdb',{
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch((error) => console.log(`DB Connection Error: ${error.message}`));
 
 const server = new ApolloServer({schema});
 
